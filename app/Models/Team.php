65208f0104dbf6e1223cbfb2d109b7b101546787
ZoranMarkovic87 <zoran.markovic87@hotmail.com>
2026-04-23 11:44:31 +0200

    -add Team model

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Team extends Model
 {
-    //
+    protected $fillable = [
+        'api_id',
+        'competition_id',
+        'name',
+        'short_name',
+        'tla',
+        'crest',
+    ];
+
+    public function competition(): BelongsTo
+    {
+        return $this->belongsTo(Competition::class);
+    }
 }
